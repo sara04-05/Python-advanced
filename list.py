193d4949cc0e727070beff3c31f1cea37b88614a
sara04-05 <Sarasimnica@outlook.com>
2026-02-10 17:48:04 +0100

    commit

---
+++ list.py
@@ -0,0 +1,15 @@
+fav_authors_list = ["kafka" , "dostoevsky" , "tolstoy" , "camus"]
+author1= fav_authors_list[0]
+author2= fav_authors_list[1]
+author3= fav_authors_list[2]
+author4= fav_authors_list[3]
+
+print(author1)
+print(author2)
+print(author3)
+print(author4)
+
+shopping_list = ["clothes" , "food" , "supplies"]
+
+shopping_list.append("shoes")
+print(shopping_list)
---
+++ variables.py
@@ -0,0 +1,17 @@
+
+temperature = 38.5
+name = "Sara"
+
+print(type(temperature))
+print(type(name))
+
+age = 18
+print("my age is:" , age)
+
+age=age+1
+print("age now is:" , age)
+
+first_name = "Sara"
+last_name = "Simnica"
+full_name = first_name + " " + last_name
+print(full_name)
