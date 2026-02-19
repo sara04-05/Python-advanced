2323ebf8f18ea41dd04282140c5310e0b1d45da5
sara04-05 <Sarasimnica@outlook.com>
2026-02-19 17:18:50 +0100

    update

--- lesson 2/dictionary.py
+++ lesson 2/dictionary.py
@@ -22,3 +22,43 @@ print(values)
 
 items = contacts.items()
 print(items)
+
+contact_information ={
+    "Festa" : {
+        "phone_number" : "123-456",
+        "email" : "Festa@gmail.com",
+        "home_adress": "Bregu i Diellit",
+        "birthday" : "03/02/2003"
+    },
+    "Sara" :  {
+        "phone_number" : "123-123",
+        "email" : "Sara@gmail.com",
+        "home_adress": "Bregu i Diellit",
+        "birthday" : "24/12/2007"
+    },
+    "Liron" :  {
+        "phone_number" : "123-111",
+        "email" : "Liron@gmail.com",
+        "home_adress": "Bregu i Diellit",
+        "birthday" : "01/04/2008"
+    }
+}
+
+print(contact_information)
+
+sara_information = contact_information["Sara"]
+print(sara_information)
+
+contacts = {
+    "Festa" : ("123-456" , "Festa@gmail.com"),
+    "Sara" : ("123-123" , "Sara@gmail.com"),
+    "Liron" : ("123-111" , "Liron@gmail.com")
+}
+
+festa_info = contacts["Festa"]
+phone_number = festa_info[0]
+print(phone_number)
+email = festa_info[1]
+print(email)
+
+phone_number, email = contacts["festa"]
---
+++ lesson 2/tuples_dictionaries.py
@@ -0,0 +1,9 @@
+grades = {
+    ("Festa" , "Biology") : 5,
+    ("Melina", "Math"): 2,
+    ("Dion" , "Physics") :3.5,
+    ("Reina", "English") : 4
+}
+
+melina_math = grades[("Melina" , "Math")]
+print("Melina's grade in math is :",melina_math)
