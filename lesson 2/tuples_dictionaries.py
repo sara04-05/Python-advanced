c43986b45b8db56183bb9854e0f23418443e1300
sara04-05 <Sarasimnica@outlook.com>
2026-02-19 17:42:10 +0100

    update

--- lesson 2/tuples_dictionaries.py
+++ lesson 2/tuples_dictionaries.py
@@ -1,9 +1,17 @@
 grades = {
-    ("Festa" , "Biology") : 5,
     ("Melina", "Math"): 2,
+    ("Festa" , "Biology") : 5,
     ("Dion" , "Physics") :3.5,
     ("Reina", "English") : 4
 }
 
 melina_math = grades[("Melina" , "Math")]
 print("Melina's grade in math is :",melina_math)
+
+grades[("Dion" , "Physics")] = 3
+print(grades)
+
+keys = list(grades.keys())
+
+student, subject = keys[0]
+print(student, "'s grade in" , subject , "is " , melina_math)
